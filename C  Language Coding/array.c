cc4900436eebe77dd1a1a9b163b760ce9911ed49
Rushi0213 <156175551+Rushi0213@users.noreply.github.com>
2026-04-13 11:26:46 +0530

    basic string array code

---
+++ C  Language Coding/array.c
@@ -0,0 +1,78 @@
+// 18/2/26
+
+// print array
+
+/*#include<stdio.h>
+int main()
+{
+	int a[]={ 1,2,3,5,4};
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
+*/
+
+
+// reverse order
+
+/* #include<stdio.h>
+void main()
+{
+	int arr[]={1,2,3,4,5};
+	int i;
+	for(i=4;i>=0;i--)
+	{
+		printf("%d",arr[i]);
+	}
+}
+*/
+
+
+// print string using array 
+/* 
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter thr input:- ");
+	scanf("%d",&n);
+	char s[]={'a','b','c','d'};
+	int i;
+	for(i=0;i<4;i++)
+	{
+		if(i==n)
+		{
+		
+		printf("%c",s[i]);
+     	}
+     	else
+     	{
+     	printf(" invalid input");
+	}
+}
+*/
+
+// check the arrya is present or not 
+
+/* 
+#include<stdio.h>
+int main()
+{
+	int a[]={1,2,3,4,5};
+	int i;
+	int num=3;
+	for(i=0;i<5;i++)
+	{
+		if(a[i]==num)
+		{
+			printf("present");
+		}
+	}
+}
+*/
+
+
+
+
---
+++ C  Language Coding/basic_strin.c
@@ -0,0 +1,31 @@
+/*  19/2/26
+// print string rushi
+
+#include<stdio.h>
+int main()
+{
+	int a[]={'r','u','s','h','i'};
+
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf("%s",a[i]);
+	}
+}
+*/
+
+/* 
+// print 1 five time
+#include<stdio.h>
+int main()
+{
+	int a=1;
+
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf("%d ",a);
+	}
+}
+*/
+
